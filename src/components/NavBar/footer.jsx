c7c918cda497eef62c021bb19ff5a04672836d61
Madhumita Jha <jha.madhumita@outlook.com>
2024-02-27 19:47:49 +0530

    frontend

---
+++ src/components/NavBar/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles.css';
+const Footer = () => {
+    return (
+        <div className='footer'>
+           <p className='rights'> &copy;2024 Books Private Limited. All rights reserved.</p> 
+        </div>
+    );
+};
+
+export default Footer;
